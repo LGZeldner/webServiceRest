4f582e8d91ec2a74216c90765610945f382f1e74
LGZeldner <48754349+LGZeldner@users.noreply.github.com>
2021-01-29 16:42:55 +0300

    Delete filter-phones-article.pipe.spec.ts

--- src/app/shared/pipes/filter-phones-article.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FilterPhonesArticlePipe } from './filter-phones-article.pipe';
-
-describe('FilterPhonesArticlePipe', () => {
-  it('create an instance', () => {
-    const pipe = new FilterPhonesArticlePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
